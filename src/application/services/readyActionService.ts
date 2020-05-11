c8ec96333a7d894d58450a07615cdb9c1499d937
RafaelGSS <rafael.nunu@hotmail.com>
2020-05-10 21:26:17 -0300

    update(readyActionService): remove lint skip

--- src/application/services/readyActionService.ts
+++ src/application/services/readyActionService.ts
@@ -1,5 +1,4 @@
 import { Client as DiscordClient } from 'discord.js';
-// eslint-disable-next-line no-unused-vars
 import { BotAction } from '../botAction';
 import { ConsoleLogger } from '../../infra/log/consoleLogger';
 
